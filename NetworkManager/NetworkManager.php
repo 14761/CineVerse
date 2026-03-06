89bd4d9456c9b19c9dbb4ec2171689953797ef43
Rafael Cadena <14761@ait.nsw.edu.au>
2026-03-06 19:11:50 +1100

    .env.example added

--- NetworkManager/NetworkManager.php
+++ NetworkManager/NetworkManager.php
@@ -1,8 +1,8 @@
 <?php
 
 declare(strict_types=1);
-require_once('vendor/autoload.php');
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+require_once(__DIR__ . '/../vendor/autoload.php');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 
@@ -13,10 +13,10 @@ class NetworkManager {
     public function __construct()
     {
         // $apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NTU3NzkxYmZkYTZjMDBjYzdlZDdhYzI3YzAxZDNjMiIsIm5iZiI6MTc3MTk5NjQ3MC40Miwic3ViIjoiNjk5ZTg1MzZhMjk4OTVmYTAzZjIyOWJmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.J3Ycks23if9YvPPW3U4IMyHbvADdUF1-USvQkESJV1A';
-        $apiKey = $_ENV['API_KEY'];
+        $this->apiKey = $_ENV['API_KEY'];
     }
     
-    function getTrendingMovies() {
+    public function getTrendingMovies() {
         
         $client = new \GuzzleHttp\Client();
 
--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 <body>
 <h1>My first PHP page</h1>
 <?php
-    require __DIR__ . '/NetworkManager/NetworkManager.php';
+    require_once __DIR__ . '/NetworkManager/NetworkManager.php';
 
     echo "Hello, World! Welcome to XAMPP.";
     $networkManager = new NetworkManager();
