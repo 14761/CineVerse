19a559f7f582da6a315937a40a494765478364b1
Rafael Cadena <14761@ait.nsw.edu.au>
2026-03-06 18:46:57 +1100

    added gitignore

--- NetworkManager/NetworkManager.php
+++ NetworkManager/NetworkManager.php
@@ -2,14 +2,18 @@
 
 declare(strict_types=1);
 require_once('vendor/autoload.php');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 
 class NetworkManager {
 
-    private string $apiKey;
+    private $apiKey;
 
     public function __construct()
     {
-        $apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NTU3NzkxYmZkYTZjMDBjYzdlZDdhYzI3YzAxZDNjMiIsIm5iZiI6MTc3MTk5NjQ3MC40Miwic3ViIjoiNjk5ZTg1MzZhMjk4OTVmYTAzZjIyOWJmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.J3Ycks23if9YvPPW3U4IMyHbvADdUF1-USvQkESJV1A';
+        // $apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NTU3NzkxYmZkYTZjMDBjYzdlZDdhYzI3YzAxZDNjMiIsIm5iZiI6MTc3MTk5NjQ3MC40Miwic3ViIjoiNjk5ZTg1MzZhMjk4OTVmYTAzZjIyOWJmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.J3Ycks23if9YvPPW3U4IMyHbvADdUF1-USvQkESJV1A';
+        $apiKey = $_ENV['API_KEY'];
     }
     
     function getTrendingMovies() {
