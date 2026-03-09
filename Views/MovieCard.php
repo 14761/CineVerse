bc233056ab4dc464786a17ee67cb3e1b6d4064f3
Phelippe <pheambrosio@gmail.com>
2026-03-09 16:09:25 +1100

    added pages movies details and JS file

--- Views/MovieCard.php
+++ Views/MovieCard.php
@@ -2,7 +2,8 @@
 require_once __DIR__ . '/../helpers/functions.php';
 
 
-function echo_movie_card($movie) {
+function echo_movie_card($movie)
+{
   echo "<div class='card' style='width: 16rem;'>";
   echo "<img src='https://image.tmdb.org/t/p/w342" . e($movie['poster_path']) . "' class='card-img-top' alt='" . e($movie['title']) . "'>";
   echo "<div class='card-body'>";
---
+++ Views/movie-details.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies - CineVerse</title>
+
+    <!-- Google Fonts for Modern Aesthetic -->
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="../style.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">CineVerse</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="../Views/index.php" class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="../Views/movies.php" class="nav-link" href="#">Movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="../Views/shows.php" class="nav-link" href="#">TV Shows</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2 search" type="search" placeholder="Search" aria-label="Search" />
+                </form>
+                <a href="../Views/login.php" class="bt-login">Login</a>
+                <a href="../Views/register.php" class="btn-primary">Register</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="movie-details">
+        <div class="movie-header">
+            <div class="movie-header-content">
+                <div class="movie-image">
+                    <img src="../Views/images/img_sample_movie_cover.jpeg" alt="Movie Poster">
+                </div>
+                <h2 class="movie-title">
+                    Movie Name
+                </h2>
+            </div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/sGbxmsDFVnE?si=DsykfgKOIDjSUBys"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+        <div class="movie-info">
+            <div class="movie-genre">
+                <p>Genre</p>
+            </div>
+            <div class="movie-release-date">
+                <p>Release Date</p>
+            </div>
+            <div class="movie-duration">
+                <p>Duration</p>
+            </div>
+        </div>
+        <div class="movie-rating">
+            <p>Rating</p>
+        </div>
+    </div>
+
+
+    <div class="join-community">
+        <h2>
+            Join Our Community
+        </h2>
+        <p>Create your account to rate movies, leave comments, and build your personalized watchlist.</p>
+        <a href="../Views/register.php" class="btn-primary">Register</a>
+    </div>
+
+    <div class="footer">
+        <p>© 2026 CineVerse. All rights reserved.</p>
+        <p>Your personal movie and TV show discovery platform</p>
+    </div>
+
+
+    <script src="../helpers/script.js"></script>
+</body>
+
+</html>
