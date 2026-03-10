ab1314cda34aa880ec71bbf069d7d84fb13afb09
Phelippe <pheambrosio@gmail.com>
2026-03-10 14:20:59 +1100

    feat: Implement a new image carousel on the homepage, adding new images and corresponding styles.

--- Views/index.php
+++ Views/index.php
@@ -13,13 +13,11 @@ require_once __DIR__ . '/../Views/MovieCard.php';
     <!-- Google Fonts for Modern Aesthetic -->
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
-    <!-- Custom Styles -->
     <link rel="stylesheet" href="../style.css">
 
 </head>
@@ -59,6 +57,28 @@ require_once __DIR__ . '/../Views/MovieCard.php';
         <h2 class="trending-title">
             <i class="bi bi-graph-up-arrow"></i>Trending Movies
         </h2>
+
+        <div id="carouselExample" class="carousel slide index-carousel-container">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../Views/images/avengers.jpeg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="../Views/images/avatar.webp" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="../Views/images/batman.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
         <div class="row">
             <?php
 
@@ -132,6 +152,7 @@ require_once __DIR__ . '/../Views/MovieCard.php';
         <p>Your personal movie and TV show discovery platform</p>
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../helpers/script.js"></script>
 </body>
 
